2c430e88abb4cc245cb6414b5a62b0e6c7934bb2
Ghiftee <gigibetine@gmail.com>
2021-11-15 15:23:36 +0100

    Add schema

--- db/migrate/20211115141233_create_recipe_foods.rb
+++ db/migrate/20211115141233_create_recipe_foods.rb
@@ -1,7 +1,9 @@
 class CreateRecipeFoods < ActiveRecord::Migration[6.1]
   def change
     create_table :recipe_foods do |t|
-
+      t.integer :quantity
+      t.references :recipe, null: false, foreign_key: true
+      t.references :food, null: false, foreign_key: true
       t.timestamps
     end
   end
