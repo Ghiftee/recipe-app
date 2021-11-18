88ef73b328d93e78d8d2e84670bad1ce719abea6
denisdiaconu <denisdiaconu117@yahoo.com>
2021-11-18 07:15:22 +0100

    Add associations and devise views

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,5 @@
 class Food < ApplicationRecord
   belongs_to :user
+  has_many :recipe_foods
+  has_many :inventory_foods
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :recipes
+  has_many :foods
+  has_many :inventories
+
+  validates :name, presence: true
 end
