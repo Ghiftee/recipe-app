bbf7ff80e6aadcfb0fcd634991410cfbf7677c04
Ghiftee <gigibetine@gmail.com>
2021-11-19 08:26:15 +0100

    Add dependent destroy

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,6 @@
 class Food < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
-  has_many :inventory_foods
+  has_many :inventory_foods, dependent: :destroy
   validates :name, :measurement_unit, :price, presence: true
 end
