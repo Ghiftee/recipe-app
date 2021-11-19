d6c08a17a2afc0c12be5b174e06c647ae12969e0
Ghiftee <gigibetine@gmail.com>
2021-11-19 08:12:52 +0100

    Add validations

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,4 +2,5 @@ class Food < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
   has_many :inventory_foods
+  validates :name, :measurement_unit, :price, presence: true
 end
--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,4 +1,6 @@
 class RecipeFood < ApplicationRecord
   belongs_to :food
   belongs_to :recipe
+  validates :food_id, presence: true
+  validates :quantity, presence: true, numericality: { greater_than: 0 }
 end
