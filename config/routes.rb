a5e918c62b6b5d36c5e967654661b9dca046239d
Ghiftee <gigibetine@gmail.com>
2021-11-19 06:41:40 +0100

    Style food view

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   end
 
   resources :recipe_foods, only: %i[edit update destroy create]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
